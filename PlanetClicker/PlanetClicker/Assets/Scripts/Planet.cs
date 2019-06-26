31d6e958103cf733770680d576843bde56f6f816
Tanya Dubinskaya <tatosh251@gmail.com>
2019-06-26 04:07:57 +0300

    first part

---
+++ PlanetClicker/PlanetClicker/Assets/Scripts/Planet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public GameObject planet;
+    [SerializeField] private Vector3 minScale;
+    [SerializeField] private Vector3 maxScale;
+    [SerializeField] private Material[] materials;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = minScale;
+        if (materials.Length != 0)
+        {
+            Material material = materials[Random.Range(0, materials.Length)];
+            planet.GetComponent<SpriteRenderer>().material = materials[Random.Range(0, materials.Length)];
+        }
+    }
+
+    void Update()
+    {
+        if (transform.localScale.x < maxScale.x)
+        {
+            transform.localScale += new Vector3(0.001F, 0.001F, 0);
+        }
+        else
+        {
+            Destroy(planet);
+        }
+    }
+
+    public void OnMouseUpAsButton()
+    {
+        Destroy(planet);
+    }
+}
---
+++ PlanetClicker/PlanetClicker/Assets/Scripts/SpawnPlanet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlanet : MonoBehaviour
+{
+    public GameObject planet;
+    void Update()
+    {
+        StartCoroutine(Spawn());   
+    }
+
+    IEnumerator Spawn() {
+        Instantiate(planet, new Vector3(Random.Range(-2.5f, 2.5f), Random.Range(-4.5f, 4.5f), 0), Quaternion.identity);
+        yield return new WaitForSeconds(5f);
+    }
+}
